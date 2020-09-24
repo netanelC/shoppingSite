f91580bcf891830b536cacc571578ffb493ddcb6
netanel96 <45965819+netanel96@users.noreply.github.com>
2020-09-24 21:26:28 +0300

    Fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
+import {BrowserRouter, Route,Link} from 'react-router-dom';
+import ProductScreen from './Screens/ProductScreen';
+import HomeScreen from './Screens/HomeScreen';
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<BrowserRouter>
+    <div className="grid-container">
+        <header className="header">
+            <div className="brand">
+                <button onClick={openMenu}>
+                    &#9776;
+                </button>
+                <Link to="/" >amazona</Link>
+            </div>
+            <div className="header-links">
+                <a href="cart.html">Cart</a>
+                <a href="signing">Sign In</a>
+            </div>
+        </header>
+        <aside className="sidebar">
+            <h3>Shopping categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+                <li>
+                    <a href="index.html">Pants</a>
+                </li>
+                <li>
+                    <a href="index.html">Shirts</a>
+                </li>
+            </ul>
+        </aside>
+        <main className="main">
+            <div className="content">
+              <Route path="/product/:id"  component={ProductScreen}/>
+              <Route path="/" exact={true} component={HomeScreen}/>
+                
+            </div>
+            
+        </main>
+        <footer className="footer">
+            All rights reserved.
+        </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,49 @@
+export default {
+    products: [{
+        _id:'1',
+        name: 'Slim Shirt0',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name: 'Slim Shirt1',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },{
+        _id:'3',
+        name: 'Slim Shirt2',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },{
+        _id:'4',
+        name: 'Fat Pant',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },{
+        _id:'5',
+        name: 'Slim Pant',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    }]
+}
